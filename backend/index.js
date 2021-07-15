15e734ad117863c6057f62dd96a647bd6c117f19
thyiuaa <thyiuaa@protonmail.com>
2021-07-15 11:39:10 +0800

    uibuild

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,13 @@
 require('dotenv').config()
-const { json, response } = require('express')
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const Person = require('./models/person')
 const app = express()
 
+//app.use(express.static('build'))
+app.use(cors())
 app.use(express.json())
-app.use(express.static('build'))
 app.use(morgan((tokens, req, res) => {
     // console.log(tokens.method(req, res), typeof tokens.method(req, res));
     // console.log(tokens.status(req, res), typeof tokens.status(req, res));
@@ -48,39 +49,29 @@ app.get('/api/persons/:id', (request, response, next) => {
 })
 
 // create a new entry
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
     const newPerson = new Person({
         name: body.name,
         number: body.number
     })
-    Person.find({ name: newPerson.name })
+    
+    newPerson.save()
         .then(result => {
-            if ( !result ) {
-                newPerson.save()
-                    .then(result => {
-                        response.json(result)
-                    })
-            } else {
-                Person.findByIdAndUpdate(request.params.id, note, { new: true })
-                    .then(result => {
-                        response.json(result)
-                    })
-                    .catch(error => next(error))
-            }
+            response.json(result)
         })
+        .catch(error => next(error))
 })
 
 // update a specific entry
-app.put('/api/notes/:id', (request, response, next) => {
+app.put('/api/persons/:id', (request, response, next) => {
     const body = request.body
-
     const person = {
       name: body.name,
       number: body.number
     }
-  
-    Person.findByIdAndUpdate(request.params.id, person)
+    
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
       .then(result => {
         response.json(result)
       })
@@ -96,6 +87,7 @@ app.delete('/api/persons/:id', (request, response, next) => {
         .catch(error => next(error))
 })
 
+// get number of entries and current time
 app.get('/info', (request, response) => {
     Person.find({})
         .then(result => {
@@ -110,26 +102,21 @@ app.get('/info', (request, response) => {
 })
 
 const errorHandler = (error, request, response, next) => {
+    console.error(error.name)
     console.error(error.message)
-    
-    if (error) {
-        if (error.name === 'CastError') {
-        return response.status(400).send({ error: 'malformatted id' })
-        }
+
+    if (error.name === 'CastError') {
+        return response.status(400).json({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(500).json({ error: error.message })
     }
 
-    if (response) {
-        const body = request.body
-        if ( !body.name || !body.number ) {
-            return response.status(400).json({ error: 'name or number missing' })
-        }
-    }
     next(error)
 }
 app.use(errorHandler)
 
 const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
+    response.status(404).json({ error: 'unknown endpoint' })
 }
 
 app.use(unknownEndpoint)
--- backend/models/person.js
+++ backend/models/person.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 
 const url = process.env.MONGODB_URI
 
@@ -14,10 +15,20 @@ mongoose.connect(url, { useNewUrlParser:true, useUnifiedTopology:true, useFindAn
   })
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String
+  name: {
+    type: String,
+    minLength: 3,
+    required: true,
+    unique: true
+  },
+  number: {
+    type: String,
+    minLength: 8,
+    required: true,
+  }
 })
 
+personSchema.plugin(uniqueValidator)
 
 personSchema.set('toJSON', {
   transform: (document, returnedObj) => {
